2e416851b9be10875ff15fc332d91d900670c615
yystudy <85561282+yu4572001@users.noreply.github.com>
2023-05-07 11:28:39 +0800

    update
    更新文件目录

--- com/yystudy/houserent/utils/Test.java
+++
@@ -1,8 +0,0 @@
-//package com.yysduty.houserent.utils;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//        String s1 = Utility.readString(10,"ikun");
-//        System.out.println(s1);
-//    }
-//}
---
+++ house_rent/src/com/yystudy/houserent/HouseRentApp.java
@@ -0,0 +1,12 @@
+package com.yysduty.houserent;
+
+import com.yysduty.houserent.view.HouseView;
+
+public class HouseRentApp {
+    public static void main(String[] args) {
+        //程序入口
+        new HouseView().mainMenu();
+
+
+    }
+}
